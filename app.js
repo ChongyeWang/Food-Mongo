c62d34dcafc67495b995bd4f62d097ffb8a28f24
Chongye jWang <wangchongye@126.com>
2020-10-14 00:15:49 -0700

    update router

--- app.js
+++ app.js
@@ -9,7 +9,10 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-app.use(express.static(path.join(__dirname, './client/build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.get('*', function(req, res) {
+	res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 app.use(logger('dev'));
 app.use(express.json());
